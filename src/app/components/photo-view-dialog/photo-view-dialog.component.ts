857f5beaaa808b31b05add82fbf32be74b96dbdc
Pawel Durczak <durczakpawel24@gmail.com>
2025-04-04 19:26:33 +0200

    Added download photo feature to photo dialog view

--- src/app/components/photo-view-dialog/photo-view-dialog.component.ts
+++ src/app/components/photo-view-dialog/photo-view-dialog.component.ts
@@ -8,6 +8,8 @@ import {
   removeFromFavorites,
 } from '../../store/actions/favorites.action';
 import { FavoritesState } from '../../store/reducers/favorites.reducer';
+import { HttpClient } from '@angular/common/http';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-photo-view-dialog',
@@ -18,10 +20,13 @@ import { FavoritesState } from '../../store/reducers/favorites.reducer';
 export class PhotoViewDialogComponent {
   photo = signal<Photo>({} as Photo);
   liked = signal(false);
+  safeImageUrl: SafeUrl | null = null;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private favoritesStore: Store<{ favorites: { photoIds: number[] } }>
+    private favoritesStore: Store<{ favorites: { photoIds: number[] } }>,
+    private http: HttpClient,
+    private sanitizer: DomSanitizer
   ) {
     this.photo.set(data.photo);
   }
@@ -32,6 +37,24 @@ export class PhotoViewDialogComponent {
       .subscribe((state: FavoritesState) => {
         this.liked.set(state.photoIds.includes(this.photo().id));
       });
+  }
+
+  downloadPhoto() {
+    this.http.get(this.photo().url, { responseType: 'blob' }).subscribe(
+      (blob: Blob) => {
+        const a = document.createElement('a');
+        const url = URL.createObjectURL(blob);
+        a.href = url;
+        a.download = this.photo().title || 'pobrany_obraz';
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+        URL.revokeObjectURL(url);
+      },
+      (error) => {
+        console.error('Wystąpił błąd podczas pobierania obrazka:', error);
+      }
+    );
   }
 
   likeOrDislikePhoto() {
